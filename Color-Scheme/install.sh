ff2fd4c513af58cf8abbed9904cc6ecb853de787
Ashikur Rahman Shad <shadgood@gmail.com>
2024-08-23 10:37:40 +0600

    Color scheme added

---
+++ Color-Scheme/install.sh
@@ -0,0 +1,1 @@
+cp -f ObsidianGlass.colors $HOME/.local/share/color-schemes/
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 cd Kvantum-Theme && sh install.sh
 cd ../Window-Decoration-Aurorae && sh install.sh
 cd ../Latte && sh install.sh
+cd ../Plasma-Style && sh install.sh
+cd ../Color-Scheme && sh install.sh
